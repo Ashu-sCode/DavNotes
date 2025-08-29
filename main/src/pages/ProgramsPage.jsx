9e353a0a195bc86c47a85844fd8dce2cd21ffdbe
Ashu-sCode <att567893393@gmail.com>
2025-08-29 17:09:31 +0530

    improvment in code

--- main/src/pages/ProgramsPage.jsx
+++ main/src/pages/ProgramsPage.jsx
@@ -21,7 +21,7 @@ const ProgramsPage = () => {
   const [search, setSearch] = useState("");
   const navigate = useNavigate();
 
-  // ---- Load from cache on mount ----
+  // ---- Loa from cache on mount ----
   useEffect(() => {
     const cached = localStorage.getItem(CACHE_KEY);
     if (cached) {
